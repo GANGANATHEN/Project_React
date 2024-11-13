707c266bdf5b05ec748506f360f5b7cee5f5bf90
GREENGANGA <GreeN11.dev@gmail.com>
2024-11-13 14:22:45 +0530

    loader added

--- Apple_clone/src/utils/index.js
+++ Apple_clone/src/utils/index.js
@@ -11,6 +11,15 @@ import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
 import exploremv from "/assets/videos/explore.mp4";
 import framemv from "/assets/videos/frame.mp4";
 
+export const heroVideo = hmv;
+export const smallHeroVideo = smallmv;
+export const highlightFirstVideo = highlightFirstmv;
+export const highlightSecondVideo = highlightSectmv;
+export const highlightThirdVideo = highlightThirdmv;
+export const highlightFourthVideo = highlightFourthmv;
+export const exploreVideo = exploremv;
+export const frameVideo = framemv;
+
 import apple from "/assets/images/apple.svg";
 import search from "/assets/images/search.svg";
 import bag from "/assets/images/bag.svg";
@@ -19,6 +28,15 @@ import right from "/assets/images/right.svg";
 import replay from "/assets/images/replay.svg";
 import play from "/assets/images/play.svg";
 import pause from "/assets/images/pause.svg";
+
+export const appleImg = apple;
+export const searchImg = search;
+export const bagImg = bag;
+export const watchImg = watch;
+export const rightImg = right;
+export const replayImg = replay;
+export const playImg = play;
+export const pauseImg = pause;
 
 import yellow from "/assets/images/yellow.jpg";
 import blue from "/assets/images/blue.jpg";
@@ -29,23 +47,6 @@ import explore2 from "/assets/images/explore2.jpg";
 import chip from "/assets/images/chip.jpeg";
 import frame from "/assets/images/frame.png";
 
-export const heroVideo = hmv;
-export const smallHeroVideo = smallmv;
-export const highlightFirstVideo = highlightFirstmv;
-export const highlightSecondVideo = highlightSectmv;
-export const highlightThirdVideo = highlightThirdmv;
-export const highlightFourthVideo = highlightFourthmv;
-export const exploreVideo = exploremv;
-export const frameVideo = framemv;
-
-export const appleImg = apple;
-export const searchImg = search;
-export const bagImg = bag;
-export const watchImg = watch;
-export const rightImg = right;
-export const replayImg = replay;
-export const playImg = play;
-export const pauseImg = pause;
 
 export const yellowImg = yellow;
 export const blueImg = blue;
